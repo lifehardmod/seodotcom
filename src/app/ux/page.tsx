466e4aee455f9c5ea68f78906c3eafdddb31d8fe
서주원 <kciimnida@gmail.com>
2025-04-24 12:52:57 +0900

    uxui 아이틀 추가

--- src/app/ux/page.tsx
+++ src/app/ux/page.tsx
@@ -1,5 +1,32 @@
+import MovieLayout from "@/components/common/MovieLayout";
+
 const Ux = () => {
-  return <div>나 UX</div>;
+  return (
+    <div>
+      <MovieLayout
+        movieLink="/movies/UX_Title.webm"
+        text={
+          <div className="flex flex-row md:gap-3 sm:gap-2 font-bold sm:text-3xl py-1 px-2 bg-black/20 text-base md:text-4xl lg:text-5xl gap-1 lg:gap-4 items-center whitespace-nowrap flex-wrap ">
+            {[
+              { bold: "개발", normal: "과" },
+              { bold: "디자인", normal: "의" },
+              { bold: "균형", normal: "을" },
+              { bold: "", normal: "고려한" },
+              { bold: "디자인", normal: "을" },
+              { bold: "", normal: "합니다." },
+            ].map((item, index) => (
+              <div key={index} className="flex">
+                {item.bold && <h1>{item.bold}</h1>}
+                <h1 className="font-normal  whitespace-nowrap">
+                  {item.normal}
+                </h1>
+              </div>
+            ))}
+          </div>
+        }
+      />
+    </div>
+  );
 };
 
 export default Ux;
